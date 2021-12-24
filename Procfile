b6f7e0323bf3cd9839d7c088bb2100b6f128cda9
habtamu <habtetsegaye19@gmail..com>
2021-12-25 00:22:15 +0300

    horeku app deployed

---
+++ Procfile
@@ -0,0 +1,5 @@
+~web: gunicorn config.wsgi --log-file 
+
+
+
+
--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '37qqvz_zxzk%hm=+#4$*vd%c&&lpk#me!q&t29^xue!1&1j)bi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
